1e0652acc3158ac9c9dc9524d092e5282b86d6e4
mlverwil <maxim.verwilst@ugent.be>
2021-05-02 19:25:05 +0200

    Implemented: authentication for user - now the user needs to provide the right password implemented: authorisation - users can be denied access to certain devices
    Added 2 example users: one with a wrong password, one with less authorised access
    
    Added test file: test_authorisation_authentication.py

--- Messages/RegisterControlApplicationMessage.py
+++ Messages/RegisterControlApplicationMessage.py
@@ -1,9 +1,13 @@
 class RegisterControlApplicationMessage(object):
     #SENT FROM CONTROL APPLICATION TO CONTROL PLATFORM
     
-    def __init__(self, application_name, encryption_type="no_encryption", encryption_args=[]):
+    def __init__(self, application_name, credentials, authorisation_level, encryption_type="no_encryption", encryption_args=[]):
         # string
         self.application_name = application_name
+        # string
+        self.credentials = credentials
+        # string
+        self.authorisation_level = authorisation_level
 
         # THE ENCRYPTION SUPPORTED BY THIS CONTROL APPLICATION IS INCORPORATED IN THE MESSAGE
         # string
@@ -12,7 +16,7 @@ class RegisterControlApplicationMessage(object):
         self.encryption_args = encryption_args
 
     def __str__(self):
-        return type(self).__name__ + '|' + 'APPLICATION_NAME=' + self.application_name + '|' + 'ENCRYPTION_DETAILS=' + self.encryption_type + ', ' + str(self.encryption_args)
+        return type(self).__name__ + '|' + 'APPLICATION_NAME=' + self.application_name + 'CREDENTIALS=' + self.credentials + '|' + 'ENCRYPTION_DETAILS=' + self.encryption_type + ', ' + str(self.encryption_args)
 
     def __repr__(self):
         return self.__str__()
