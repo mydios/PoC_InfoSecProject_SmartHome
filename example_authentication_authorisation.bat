70f1c0e79d69ce272f7b5555e9499d6218abe26d
rschryer <rens.schryers@ugent.be>
2021-06-10 17:02:29 +0200

    small update

--- example_authentication_authorisation.bat
+++ example_authentication_authorisation.bat
@@ -2,4 +2,5 @@ start cmd /k python network.py
 start cmd /k python example_cp.py 
 start cmd /k python example_lamp.py 
 start cmd /k python wrong_credentials_ca.py 
-start cmd /k python correct_credentials_ca.py 
+start cmd /k python guest_credentials_ca.py 
+start cmd /k python example_ca.py
--- simple_lamp.bat
+++ simple_lamp.bat
@@ -1,3 +1,4 @@
 start cmd /k python network.py 
 start cmd /k python example_cp.py 
 start cmd /k python example_lamp.py 
+start cmd /k python example_ca.py
